eaf125c386353f6fd8671dc5f16942de2c6a823e
Squirrel68 <constantlypeacinside@gmail.com>
2023-09-01 17:15:36 +0700

    add dotenv module

--- src/apps/app.js
+++ src/apps/app.js
@@ -2,27 +2,34 @@ const express = require("express");
 const app = express();
 const config = require("config");
 const session = require("express-session");
-//Seesion
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: config.get('app.session_key'),
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+const dotenv = require("dotenv");
+
+//dotenv
+dotenv.config({ path: `${__dirname}/../../config.env` });
+
+//Session
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: config.get("app.session_key"),
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 //data
 app.use("/static", express.static(config.get("app.static_folder")));
 //Form
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //View
 app.set("views", config.get("app.view_folder"));
-app.set("view engine", config.get("app.view_engine"));
+app.set("view engine", "ejs");
 //Account
 app.use(require("./middlewares/account"));
 //Category
 app.use(require("./middlewares/share"));
-//Cart 
+//Cart
 app.use(require("./middlewares/cart"));
 //Router
 app.use(require(config.get("app.router")));
--- src/bin/www.js
+++ src/bin/www.js
@@ -1,5 +1,9 @@
+const dotenv = require("dotenv");
+
 const app = require(`${__dirname}/../apps/app.js`);
-const config = require('config');
-app.listen(port = config.get('app.port'), (req, res)=>{
-    console.log(`Server running on port ${port}`);
+const config = require("config");
+
+const port = process.env.PORT || 5000;
+app.listen(port, (req, res) => {
+  console.log(`Server running on port ${port}`);
 });
